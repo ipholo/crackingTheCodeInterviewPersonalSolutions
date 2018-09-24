5609691283b4f06a1ea97b753fdbc52640097519
leopoldohernandez <leopoldohernandez@192.168.1.72>
2018-09-24 07:17:47 -0500

    Modify tests where expected variable was redundant.

--- test/crackingthecodinginterviewpersonalsolutions/arraysAndStringsSolutions/StaticMethodsTest.java
+++ test/crackingthecodinginterviewpersonalsolutions/arraysAndStringsSolutions/StaticMethodsTest.java
@@ -292,13 +292,18 @@ public class StaticMethodsTest {
     
     @Test
     public void testSetRowAndColumZeroWithElementZero_assertResult_4x4NoZerosMatrix() {
+        int[][] matrix =
+            {{1,   2,  3,  4},
+             {5,   6,  7,  8},
+             {9,  10, 11, 12},
+             {13, 14, 15, 16}};
         int[][] expected =
             {{1,   2,  3,  4},
              {5,   6,  7,  8},
              {9,  10, 11, 12},
              {13, 14, 15, 16}};
         
-        int[][] result = setRowAndColumZeroWithElementZero(expected);
+        int[][] result = setRowAndColumZeroWithElementZero(matrix);
         
         assertArrayEquals(expected, result);
     }
